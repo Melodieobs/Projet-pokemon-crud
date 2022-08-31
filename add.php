896cd1fc5e991a72754f858f99efe2d9933acf36
unknown <melodieobszynski@hotmail.com>
2022-08-31 23:05:40 +0200

    correctionverifFormAdd

--- add.php
+++ add.php
@@ -11,17 +11,21 @@ if($connexion){
     $pkmn_release_date = $_POST["d-de-sortie"];
     $pkmn_support = $_POST["support-sortie"];
 
-    $execResult = $connexion->query ("CREATE TABLE IF NOT EXISTS `pokemon_games`(
-        `pkmn_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-        `pkmn_game_name` varchar(255) DEFAULT NULL,
-        `pkmn_generation` int(11) DEFAULT NULL,
-        `pkmn_release_date` DATE DEFAULT NULL,
-        `pkmn_support` varchar(255) DEFAULT NULL,
-        `pkmn_image` varchar(255) DEFAULT NULL,
-        PRIMARY KEY (`pkmn_id`),
-        UNIQUE KEY `pkmn_id` (`pkmn_id`))");
+    $execResult = $connexion->query("CREATE DATABASE IF NOT EXISTS 'pokemon-crud'");
 
-    $execResult = $connexion->query("INSERT INTO pokemon_games (pkmn_game_name,pkmn_generation,pkmn_release_date,pkmn_support) VALUE ('$pkmn_game_name',$pkmn_generation,$pkmn_release_date,'$pkmn_support')");
+    $execResult = $connexion->query("CREATE TABLE IF NOT EXISTS 'pokemon_games'(
+        'pkmn_id' bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+        'pkmn_game_name' varchar(255) DEFAULT NULL,
+        'pkmn_generation' int(11) DEFAULT NULL,
+        'pkmn_release_date' DATE DEFAULT NULL,
+        'pkmn_support' varchar(255) DEFAULT NULL,
+        'pkmn_image' varchar(255) DEFAULT NULL,
+        PRIMARY KEY ('pkmn_id'),
+        UNIQUE KEY 'pkmn_id' ('pkmn_id'))");
+
+        
+
+    // $execResult = $connexion->query("INSERT INTO pokemon_games (pkmn_game_name,pkmn_generation,pkmn_release_date,pkmn_support) VALUE ('$pkmn_game_name',$pkmn_generation,$pkmn_release_date,'$pkmn_support')");
 
     // var_dump($pkmn_game_name);
     // Parcours du tableau de résultats (afin d'afficher chaque utilisateur)
--- formlaire-ajout.php
+++ formlaire-ajout.php
@@ -1,7 +1,75 @@
+<?php
+
+require("./jeuxPok/conn.php");
+
+$paterne = '/^[A-Za-z1-2\s&]+$/';
+$paterne2 = '/^[0-9]+$/';
+$tabval = [];
+
+//$remplaceGame_Name= str_ireplace( "é","e",$pkmn_game_name);
+
+if($connexion){   
+        foreach($_POST as $fields => $value){
+            array_push($tabval, $value);
+
+            var_dump($tabval);                
+            if($fields === "generation"){
+                $value = trim(ucfirst($value));
+                if(empty($value)){
+                    echo $errGen = "Le champs génération n'est pas remplis";
+                }
+                elseif(preg_match("$paterne2", $value)){
+                    echo $errGen = "erreur il n'y a que 8 génération";
+                }
+                else{
+                    $generation = $value;
+                }
+            }
+            if($fields === "titre"){
+                if(empty($value)){
+                    echo $errTitre = "Le champs titre n'as pas était remplis";
+                }
+                elseif(!preg_match("$paterne", $value)){
+                    echo $errTitre = "La titre saisie n'est pas bonne.";
+                    var_dump(!preg_match("$paterne", $value));
+                }
+                else{
+                    $titre = $value;
+                }
+            }
+            if($fields === "supportsortie"){
+                if(empty($value)){
+                    echo $errSuppSortie = "Le champ date de sorti n'as pas était remplis";
+                }
+                elseif(!preg_match("$paterne", $value)){
+                    echo $errSuppSortie = "La valeur du support n'est pas bonne.";
+                    var_dump(!preg_match("$paterne", $value));
+                }
+                else{
+                    $supp = $value;
+                }
+            }
+            if($fields === "ddesortie"){
+                if($value > date("Y-m-d")){
+                    echo $errDate = "La date saisie n'est pas bonne" ;
+                }
+                else{
+                    $date = $value;
+                }
+            }
+        }
+        if($date && $supp && $titre && $generation){
+            $execResult = $connexion->query("INSERT INTO pokemon_games (pkmn_game_name,pkmn_generation,pkmn_release_date,pkmn_support) VALUES ('$titre',$generation, '$date', '$supp')");          
+    }
+        else{
+            echo "Une erreurs est survenue, il y a des erreurs dans les champs";
+        }
+}
+
+?>
+
 
     <form method="POST" action="./add.php">
-
-
 <div>
     <label for="titre-poke-jeux">Titre de jeux pokémon: </label>
     <input type="text" name="titre-poke-jeux" id="titre-de-jeux" <?php if(isset($execResult)){ echo ["titre-poke-jeux"];} ?>>
