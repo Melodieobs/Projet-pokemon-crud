2911b350e481eaad459aa527df22e94265d63dc3
unknown <melodieobszynski@hotmail.com>
2022-09-03 15:17:27 +0200

    rajoutphotoverif

--- update.php
+++ update.php
@@ -8,7 +8,7 @@
     $paterne2 = '/^[0-9]+$/';
     $tabval = [];
 
-    //$remplaceGame_Name= str_ireplace( "é","e",$pkmn_game_name);
+    //$remplaceGame_Name= str_ireplace( "é","e",$pkmn_game_name)
 
     if($connexion){
         if($_GET && $_GET["pkmn_id"]){
@@ -26,18 +26,17 @@
             
             foreach($_POST as $fields => $value){
                 array_push($tabval, $value);
-
-                // var_dump($tabval);                
+                              
                 if($fields === "generation"){
                     $value = trim(ucfirst($value));
                     if(empty($value)){
-                        echo $errGen = "Le champs génération n'est pas remplis\n\n";
+                        echo $err = "Le champs génération n'est pas remplis\n\n";
                     }
                     elseif($value == 9){
-                        echo $errGen = "Désolé pour toi cette génération sort le 18/novembre/2022 ! ";
+                        echo $err = "Désolé pour toi cette génération sort le 18/novembre/2022 ! ";
                     }
                     elseif($value > 8){
-                        echo $errGen = "erreur il n'y a que 8 génération\n\n";
+                        echo $err = "erreur il n'y a que 8 génération\n\n";
                     }
                     else{
                         $generation = $value;
@@ -45,10 +44,10 @@
                 }
                 if($fields === "titre"){
                     if(empty($value)){
-                        echo $errTitre = "Le champs titre n'as pas était remplis.\n\n";
+                        echo $err = "Le champs titre n'as pas était remplis.\n\n";
                     }
                     elseif(!preg_match("$paterne", $value)){
-                        echo $errTitre = "La titre saisie n'est pas bonne.\n\n";
+                        echo $err = "La titre saisie n'est pas bonne.\n\n";
                     }
                     else{
                         $titre = $value;
@@ -56,10 +55,10 @@
                 }
                 if($fields === "supportsortie"){
                     if(empty($value)){
-                        echo $errSuppSortie = "Le champ date de sorti n'as pas était remplis.\n\n";
+                        echo $err = "Le champ date de sorti n'as pas était remplis.\n\n";
                     }
                     elseif(!preg_match("$paterne", $value)){
-                        echo $errSuppSortie = "La valeur du support n'est pas bonne.\n\n";
+                        echo $err = "La valeur du support n'est pas bonne.\n\n";
                     }
                     else{
                         $supp = $value;
@@ -67,43 +66,56 @@
                 }
                 if($fields === "ddesortie"){
                     if($value > date("Y-m-d")){
-                        echo $errDate = "La date saisie n'est pas bonne.\n\n" ;
+                        echo $err = "La date saisie n'est pas bonne.\n\n" ;
                     }
                     else{
                         $date = $value;
                     }
                 }
+                if($fields === "url"){
+                    if(filter_var($value, FILTER_VALIDATE_URL)){
+                        $url = $value;
+                    }
+                    else{
+                        echo $err = "J'ai bien l'impression que cela n'est pas un url !";
+                    }
             }
-            if(isset($date) && isset($supp) && isset($titre) && isset($generation)){
-                $execResult = $connexion->query("UPDATE pokemon_games SET pkmn_game_name = '$titre', pkmn_generation ='$generation' , pkmn_support = '$supp', pkmn_release_date ='$date' WHERE pkmn_id = '$pkmnId'");
+            if(isset($date) && isset($supp) && isset($titre) && isset($generation) && isset($url)){
+                $execResult = $connexion->query("UPDATE pokemon_games SET pkmn_game_name = '$titre', pkmn_generation ='$generation' , pkmn_support = '$supp', pkmn_release_date ='$date', pkmn_image = '$url' WHERE pkmn_id = '$pkmnId'");
                 
             }
         }
     }
+} 
 
 ?>
+
 <form method="POST" action="./update.php">
         <input type="hidden" name="pkmn_id" value="<?php if(isset($result)){ echo $_GET["pkmn_id"];} ?>">
         <div>
             <label for="titre-de-jeux">Titre de jeux pokémon : </label>
-            <input type="text" name="titre" id="titre-de-jeux" value="<?php if(isset($result)){ echo $result[0]["pkmn_game_name"];}?>">
+            <input type="text" name="titre" id="titre-de-jeux" value="<?php if(isset($result)){ echo $result[0]["pkmn_game_name"];} if(isset($err)){ echo $_POST['titre']; } ?>">
         </div>
 
         <div>
             <label for="gener">Génération : </label>
-            <input type="number" name="generation" id="gener" value="<?php if(isset($result)){ echo $result[0]["pkmn_generation"];}?>">
+            <input type="number" name="generation" id="gener" placeholder="<?php if(isset($result)){ echo $result[0]['pkmn_generation'] ;} if(isset($err)){ echo $_POST['generation']; }?> ">
         </div>
 
         <div>
             <label for="supp-sortie">Support de sortie : </label>
-            <input type="text" name="supportsortie" id="supp-sortie" value="<?php if(isset($result)){ echo $result[0]["pkmn_support"];}?>">
+            <input type="text" name="supportsortie" id="supp-sortie" value="<?php if(isset($result)){ echo $result[0]["pkmn_support"];} if(isset($err)){ echo $_POST['supportsortie']; }?>">
         </div>
 
         <div>
             <label for="date-de-sortie">Date de sortie Européenne : </label>
-            <input type="date" name="ddesortie" id="date-de-sortie" value="<?php if(isset($result)){ echo $result[0]["pkmn_release_date"];}?>">
+            <input type="date" name="ddesortie" id="date-de-sortie" value="<?php if(isset($result)){ echo $result[0]["pkmn_release_date"]; } ?>">
         </div>
-
+        <div>
+            <input type="URL" name="url" value="<?php if(isset($result)){ echo $result[0]["pkmn_image"];} if(isset($err)){ echo $_POST['url']; }?>">
+            <img src="<?php if(isset($result)){ echo $result[0]["pkmn_image"]; } ?>" alt="img" >
+        </div>
         <input type="submit" value="Modifier">
     </form>
     <a href="./read.php" title="Redirection vers la liste">Retourner à la liste pokemon</a> 
+    
