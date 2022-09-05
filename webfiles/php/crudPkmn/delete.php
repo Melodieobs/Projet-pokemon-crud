6c3e0fef5d0f38a402050ad0853428bb5bb6c74c
unknown <melodieobszynski@hotmail.com>
2022-09-05 23:01:55 +0200

    SuppressionInterditeSSSUCCES

--- webfiles/php/crudPkmn/delete.php
+++ webfiles/php/crudPkmn/delete.php
@@ -5,12 +5,20 @@
 require("../conn.php");
 
 if($connexion){
+    session_start();
+    if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     $pkmnID = $_POST['hiddenField'];
 
     $execResult = $connexion->query("DELETE FROM pokemon_games WHERE pkmn_id = $pkmnID");
     var_dump($execResult);
     header("Location: ../../../../read.php");
 }
+else { ?>
+    <h2>Vous n'avez pas l'acces de suppression !</h2>
+    <a href="../../../read.php">Page principal</a>
+<?php }
+}
+
 
 
 ?>
