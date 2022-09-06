21f43d9be8b69ed21e6a4762e08b4fe95e8d5fd1
unknown <melodieobszynski@hotmail.com>
2022-09-06 21:50:30 +0200

    UserPasaccesausuppdesautresSUCCES

--- webfiles/php/crudUser/delete.php
+++ webfiles/php/crudUser/delete.php
@@ -7,16 +7,17 @@ require("../conn.php");
 if($connexion){
     session_start();
     if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-        // if($userID == $execResult['user_id'] ){
-    $userID = $_POST['hidden'];
+        $userID = $_POST['hidden'];
+        if($userID == $_SESSION["id"]){
+    
 
     $execResult = $connexion->query("DELETE FROM users WHERE user_id = $userID");
-   header("Location: ./read.php");
-   var_dump($execResult);
-        // }
-        // else { ?>
-            <!-- <p>Malin le linx mais c'est pas ton compte MDRRR !!</p> -->
-        <?php // }
+    header("Location: ./read.php");
+         }
+     else { ?>
+            <p>Malin le lynx mais ce n'est pas ton compte MDRRR!!</p> 
+            <p>Aller respect les autres, est supprime le tien tu n'es que honte de ton existence !! <a href="./read.php" title="Les personnes comme toi ne sont pas faites pour être ici">Supprime TON COMPTE !</a> </p> 
+        <?php  }
     }
     else { ?>
         <h2>Vous n'avez pas l'acces de suppression !</h2>
